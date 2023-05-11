fdcbcc84e3e2f419b263cd3ae6c6f1a07f2bc314
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-05-11 00:48:38 -0500

    retrofit api connections

---
+++ app/src/main/java/com/example/retrofitrecyclerview/ApiInterface.kt
@@ -0,0 +1,9 @@
+package com.example.retrofitrecyclerview
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiInterface {
+    @GET("/users")
+    fun getData(): Call<List<UserItem>>
+}
---
+++ app/src/main/java/com/example/retrofitrecyclerview/UserItem.kt
@@ -0,0 +1,6 @@
+package com.example.retrofitrecyclerview
+
+class UserItem (
+    var avatar_url:String,
+    var login:String
+        )
